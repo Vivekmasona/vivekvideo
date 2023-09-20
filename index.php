81bab56df79f2e97fe62a35792626c82e21eb5c8
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-09-21 04:13:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 const express = require('express');
-const ytdl = require('ytdl-core');
 const app = express();
-const port = 3000;
+const ytdl = require('ytdl-core');
+const fs = require('fs');
 
 app.get('/download', async (req, res) => {
+  const videoUrl = req.query.url; // Get the video URL from the query parameter
+  if (!videoUrl) {
+    res.status(400).send('Please provide a valid video URL');
+    return;
+  }
+
   try {
-    const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
-    if (!videoURL) {
-      return res.status(400).send('Please provide a valid YouTube video URL.');
-    }
+    const info = await ytdl.getInfo(videoUrl);
+    const videoStream = ytdl(videoUrl, {
+      quality: 'highest',
+    });
 
-    // Get the video info to retrieve the title
-    const info = await ytdl.getInfo(videoURL);
-    const videoTitle = info.videoDetails.title;
-
-    // Set the response headers for video download
-    res.header('Content-Disposition', `attachment; filename="${videoTitle}.mp4"`);
-    ytdl(videoURL, {
-      format: 'mp4',
-    }).pipe(res);
+    res.header('Content-Disposition', `attachment; filename="${info.videoDetails.title}.mp4"`);
+    videoStream.pipe(res);
   } catch (error) {
     console.error('Error:', error);
-    res.status(500).send('An error occurred while processing your request.');
+    res.status(500).send('An error occurred');
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
