270503ad14676d661871ab79db83e7048fe0c450
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-09-22 11:06:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ app.get('/thumbnail', (req, res) => {
   // YouTube link se video ID nikale
   const videoId = extractVideoId(youtubeLink);
 
-  // Thumbnail URL banaye
-  const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+  if (videoId) {
+    // Thumbnail URL banaye
+    const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
 
-  // Thumbnail URL ko client ko bheje
-  res.send(`<img src="${thumbnailUrl}" alt="YouTube Thumbnail">`);
+    // Thumbnail URL ko client ko bheje
+    res.send(`<img src="${thumbnailUrl}" alt="YouTube Thumbnail">`);
+  } else {
+    res.send('Invalid YouTube link.');
+  }
 });
 
 // Server ko port 3000 par sunaye
@@ -22,7 +26,13 @@ app.listen(3000, () => {
 
 // YouTube link se video ID nikalne ka function
 function extractVideoId(url) {
-  // YouTube link se video ID nikalne ka logic yahan likhein
-  // Example: "https://www.youtube.com/watch?v=VIDEO_ID" se "VIDEO_ID" nikalna
-}
+  // YouTube video ID ko extract karne ke liye regular expression ka upayog karein
+  const regex = /(?:\/|%3D|v=|vi=|youtu\.be\/|\/embed\/|\/v\/|\/e\/|watch\?v=|youtube.com\/user\/[^#]*#([^\/]*?\/)*)[^#\&\?]*?([^\<\>\"\'\s]*)/;
+  const match = url.match(regex);
 
+  if (match && match[2].length === 11) {
+    return match[2]; // Extracted video ID
+  } else {
+    return null; // Invalid YouTube link
+  }
+      }
