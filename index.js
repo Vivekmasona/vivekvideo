426608205ca1dac632aa75e46babd627c4df9a0d
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-09-23 23:36:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,26 +6,24 @@ const port = 3000;
 
 app.get('/download', async (req, res) => {
   try {
-    const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
+    const videoURL = req.query.url; // Query parameter se YouTube video URL prapt karein
 
     if (!videoURL) {
-      return res.status(400).send('Missing video URL');
+      return res.status(400).send('Video URL ki kami hai');
     }
 
-    // Get information about the video (including the title)
+    // Video ke baare mein jaankari prapt karein (title shamil hai)
     const info = await ytdl.getInfo(videoURL);
     const videoTitle = info.videoDetails.title;
+    const autoTitle = videoTitle.replace(/[^\w\s]/gi, ''); // Title se special characters ko hatakar sanitized title taiyar karein
+    const sanitizedTitle = autoTitle || 'audio'; // Sanitized title ya 'audio' (agar title mein special characters the) ko default ke roop mein istemal karein
 
-    const autoTitle = videoTitle.replace(/[^\w\s]/gi, ''); // Remove special characters from the title
-    const sanitizedTitle = autoTitle || 'audio'; // Use the sanitized title or 'audio' as a default
+    // Response headers ko set karein taki ek downloadable file auto-generated title ke saath specified ho
+    res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}.mp4"`);
+    res.setHeader('Content-Type', 'video/mp4');
 
-    // Set response headers to specify a downloadable file with the auto-generated title
-    res.setHeader('Content-Disposition', `attachment; filename="vivekfy💞${sanitizedTitle}.mp3"`);
-    res.setHeader('Content-Type', 'audio/mpeg');
-    res.setHeader('Content-Length', fileSizeInBytes);
-    res.setHeader('X-Video-Length', lengthInSeconds);
-    // Pipe the audio stream into the response
-    ytdl(videoURL, { filter: 'audioonly' }).pipe(res);
+    // Video stream ko response mein pipe karein, jisse download shuru ho
+    ytdl(videoURL, { quality: 'highestvideo' }).pipe(res);
   } catch (error) {
     console.error('Error:', error);
     res.status(500).send('Internal Server Error');
@@ -33,5 +31,5 @@ app.get('/download', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server port ${port} par chalu hai`);
 });
