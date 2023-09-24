750dc92afdeadb62b73f14dd21365b9e68b6345d
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-09-24 21:37:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,31 @@ const ytdl = require('ytdl-core');
 const app = express();
 const port = 3000; // Replace with your desired port number
 
-app.get('/title', (req, res) => {
+app.get('/get', (req, res) => {
   const videoUrl = req.query.url;
 
   if (!videoUrl) {
     return res.status(400).send('Missing "url" query parameter');
   }
 
-  // Use the `ytdl.getBasicInfo` function to fetch video details, including the title.
+  // Use the `ytdl.getBasicInfo` function to fetch video details, including the title and thumbnail.
   ytdl.getBasicInfo(videoUrl, (err, info) => {
     if (err) {
       console.error('Error fetching video info:', err);
       res.status(500).send('Error fetching video info');
     } else {
-      // Access the video title from the `info` object.
+      // Access the video title and thumbnail URL from the `info` object.
       const videoTitle = info.title;
-      res.send(`Video Title: ${videoTitle}`);
+      const thumbnailUrl = info.player_response.videoDetails.thumbnail.thumbnails[0].url;
+      
+      // Create an object with video information.
+      const videoInfo = {
+        title: videoTitle,
+        thumbnailUrl: thumbnailUrl,
+      };
+      
+      // Send the video information as JSON response.
+      res.json(videoInfo);
     }
   });
 });
@@ -27,4 +36,4 @@ app.get('/title', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
+                    
