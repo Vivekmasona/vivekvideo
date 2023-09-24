06e51c1dd09e81137c7ed6993d3416845f2f10a7
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-09-24 22:19:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,26 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-// Change this line to specify the quality you want, e.g., 'highestvideo' or 'hd720'
-const qualityOption = 'hd720';
-
 app.get('/download', async (req, res) => {
   try {
-    const videoURL = req.query.url;
+    const videoURL = req.query.url; // Query parameter se YouTube video URL prapt karein
 
     if (!videoURL) {
       return res.status(400).send('Video URL ki kami hai');
     }
 
+    // Video ke baare mein jaankari prapt karein (title shamil hai)
     const info = await ytdl.getInfo(videoURL);
     const videoTitle = info.videoDetails.title;
-    const autoTitle = videoTitle.replace(/[^\w\s]/gi, '');
-    const sanitizedTitle = autoTitle || 'audio';
+    const autoTitle = videoTitle.replace(/[^\w\s]/gi, ''); // Title se special characters ko hatakar sanitized title taiyar karein
+    const sanitizedTitle = autoTitle || 'audio'; // Sanitized title ya 'audio' (agar title mein special characters the) ko default ke roop mein istemal karein
 
-    // Modify the options to specify the quality
-    const options = {
-      quality: qualityOption,
-    };
-
+    // Response headers ko set karein taki ek downloadable file auto-generated title ke saath specified ho
     res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}.mp4"`);
     res.setHeader('Content-Type', 'video/mp4');
 
-    ytdl(videoURL, options).pipe(res);
+    // Video stream ko response mein pipe karein, jisse download shuru ho
+    ytdl(videoURL, { quality: 'highestvideo' }).pipe(res);
   } catch (error) {
     console.error('Error:', error);
     res.status(500).send('Internal Server Error');
