4e5f1fd48b94a4e2aaeb243e8d15bb00a232dc88
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-09-25 19:54:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
+
+
 const express = require('express');
-const ytdl = require('ytdl-core');
-const fs = require('fs');
 const app = express();
-const port = 3000;
-const qualityOption = 'hd720';
-app.get('/download', async (req, res) => {
-  try {
-    const videoURL = req.query.url; // Query parameter se YouTube video URL prapt karein
 
-    if (!videoURL) {
-      return res.status(400).send('Video URL ki kami hai');
-    }
-
-    // Video ke baare mein jaankari prapt karein (title shamil hai)
-    const info = await ytdl.getInfo(videoURL);
-    const videoTitle = info.videoDetails.title;
-    const autoTitle = videoTitle.replace(/[^\w\s]/gi, ''); // Title se special characters ko hatakar sanitized title taiyar karein
-    const sanitizedTitle = autoTitle || 'audio'; // Sanitized title ya 'audio' (agar title mein special characters the) ko default ke roop mein istemal karein
-
-    // Response headers ko set karein taki ek downloadable file auto-generated title ke saath specified ho
-    res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}.mp4"`);
-    res.setHeader('Content-Type', 'video/mp4');
-
-    // Video stream ko response mein pipe karein, jisse download shuru ho
-    ytdl(videoURL, { quality: 'highestvideo' }).pipe(res);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+app.get('/img', (req, res) => {
+  const videoURL = req.query.url; // Get the video URL from the query parameters
+  if (!videoURL) {
+    res.status(400).send('Missing video URL');
+    return;
   }
+
+  const videoId = videoURL.split("v=")[1];
+  if (!videoId) {
+    res.status(400).send('Invalid video URL');
+    return;
+  }
+
+  const imageURL = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
+  res.json({ imageURL });
 });
 
-app.listen(port, () => {
-  console.log(`Server port ${port} par chalu hai`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
