bed49dced7fabb70f0094f57a2a62b972492baef
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-15 15:31:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the "images" directory
 app.use(express.static('images'));
 
-// Define a route to get a random image URL
-app.get('/random-image', (req, res) => {
-  // Get a list of image files in the "images" directory
-  const imageFiles = ['/img/image1.jpg', '/img/image2.jpg', 'img/image3.jpg']; // Add your image filenames here
-
-  // Generate a random index to select a random image
+app.get('/ai', (req, res) => {
+  const imageFiles = ['/img/image1.jpg', '/img/image2.jpg', '/img/image3.jpg']; // Add your image filenames here
   const randomIndex = Math.floor(Math.random() * imageFiles.length);
+  const randomImageFileName = imageFiles[randomIndex];
 
-  // Construct the URL for the random image
-  const randomImageUrl = `/images/${imageFiles[randomIndex]}`;
+  // Set the appropriate content type for the image
+  res.setHeader('Content-Type', 'image/jpeg'); // Modify this based on your image type (e.g., image/png for PNG images)
 
-  res.json({ imageUrl: randomImageUrl });
+  // Send the image file
+  res.sendFile(`${__dirname}/images/${randomImageFileName}`);
 });
 
 app.listen(port, () => {
