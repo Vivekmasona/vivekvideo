102dde76fbe36cada862093b1a1119a4ca3083d1
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-18 23:23:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+
 const app = express();
-const port = 3000; // Replace with your desired port number
+const port = 3000;
 
-app.get('/audio', (req, res) => {
-  const videoURL = req.query.url;
+app.get('/play', (req, res) => {
+    const ytLink = req.query.url;
 
-  if (!videoURL) {
-    return res.status(400).send('Please provide a valid YouTube video URL.');
-  }
+    if (!ytLink) {
+        return res.status(400).send('Missing YouTube URL');
+    }
 
-  if (ytdl.validateURL(videoURL)) {
-    const audioStream = ytdl(videoURL, {
-      quality: 'lowestaudio', // You can adjust the quality as needed
+    // Get video info
+    ytdl.getInfo(ytLink, (err, info) => {
+        if (err) {
+            return res.status(500).send('Error fetching video information');
+        }
+
+        // Get the highest quality audio stream
+        const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
+
+        if (audioFormat) {
+            const audioStream = ytdl(ytLink, { format: audioFormat });
+
+            // Create a writable stream to save the audio
+            const audioOutput = fs.createWriteStream('output.mp3');
+
+            audioStream.pipe(audioOutput);
+
+            audioOutput.on('finish', () => {
+                // Send the audio file as a response
+                res.download('output.mp3', 'audio.mp3', (err) => {
+                    if (err) {
+                        console.error('Error sending audio:', err);
+                        res.status(500).send('Error sending audio');
+                    } else {
+                        console.log('Audio extraction and response complete.');
+                    }
+                });
+            });
+
+            audioOutput.on('error', (err) => {
+                console.error('Error saving audio:', err);
+                res.status(500).send('Error saving audio');
+            });
+        } else {
+            res.status(500).send('No suitable audio format found');
+        }
     });
-
-    // Generate a Google Play Music playback link
-    const googlePlayLink = `https://play.google.com/music/m/${generateTrackId(videoURL)}`;
-
-    res.json({
-      googlePlayLink,
-      audioStreamUrl: audioStream.url,
-    });
-  } else {
-    res.status(400).send('Invalid YouTube URL');
-  }
 });
 
-function generateTrackId(videoURL) {
-  // Logic to generate a unique track ID, e.g., from the video URL
-  // You can implement your own logic to generate a track ID here
-  return 'your_generated_track_id';
-}
-
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
 
