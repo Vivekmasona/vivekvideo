33f7fa88d352ba7feb6907cc25762a1db0859b60
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-18 19:33:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,30 @@ const ytdl = require('ytdl-core');
 const app = express();
 const port = 3000;
 
-app.get('/play', async (req, res) => {
-  const youtubeUrl = req.query.url;
-  const timestamp = req.query.t || 0; // Default to 0 if no timestamp is provided
+app.get('/get-direct-audio-link', (req, res) => {
+  const videoUrl = req.query.url; // Get the YouTube video URL from the query parameter
 
-  if (!youtubeUrl) {
-    res.status(400).send('Missing YouTube URL');
-  } else {
-    try {
-      const stream = ytdl(youtubeUrl, { filter: 'audioonly', quality: 'lowestaudio', begin: timestamp });
-      res.set('Content-Type', 'audio/mpeg');
-      stream.pipe(res);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Failed to retrieve audio from YouTube URL');
-    }
+  if (!videoUrl) {
+    return res.status(400).send('Video URL is missing');
   }
+
+  ytdl.getInfo(videoUrl, (err, info) => {
+    if (err) {
+      console.error('Error getting video info:', err);
+      return res.status(500).send('Error getting video info');
+    } else {
+      const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
+      
+      if (format) {
+        res.send({ directPlaybackLink: format.url });
+      } else {
+        res.status(404).send('No audio format found');
+      }
+    }
+  });
 });
 
-app.use(express.static('public')); // Serve the HTML file
-
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
 
