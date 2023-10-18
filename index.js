337ca01e32ce0f5dd5612204e43b24657e563e4c
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-18 23:36:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const ytdl = require('ytdl-core');
-const fs = require('fs');
-
 const app = express();
-const port = 3000;
+app.use('/static', express.static('./static'));
 
-app.get('/play', (req, res) => {
-    const ytLink = req.query.url;
-
-    if (!ytLink) {
-        return res.status(400).send('Missing YouTube URL');
-    }
-
-    // Get video info
-    ytdl.getInfo(ytLink, (err, info) => {
-        if (err) {
-            return res.status(500).send('Error fetching video information');
-        }
-
-        // Get the highest quality audio stream
-        const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
-
-        if (audioFormat) {
-            const audioStream = ytdl(ytLink, { format: audioFormat });
-
-            // Set the response headers for audio playback
-            res.setHeader('Content-disposition', 'attachment; filename=audio.mp3');
-            res.setHeader('Content-type', 'audio/mpeg');
-
-            audioStream.pipe(res);
-        } else {
-            res.status(500).send('No suitable audio format found');
-        }
-    });
+app.listen(3000, () => {
+    console.log("It Works!");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: './' });
+})
+
+app.get('/download', (req, res) => {
+    var url = req.query.url;
+    res.header("Content-Disposition", 'attachment; filename="Video.mp4');
+    ytdl(url, {format: 'mp4'}).pipe(res);
 });
