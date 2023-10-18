9862c015f27059aefaa7560655aa34848bf2f30f
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-18 23:45:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express');
-const cors = require('cors');
 const ytdl = require('ytdl-core');
-const app = express();
-app.use('/static', express.static('./static'));
 
-app.listen(3000, () => {
-    console.log("It Works!");
+const app = express();
+const port = 3000;
+
+app.get('/play', (req, res) => {
+    const ytLink = req.query.url;
+
+    if (!ytLink) {
+        return res.status(400).send('Missing YouTube URL');
+    }
+
+    // Get video info
+    ytdl.getInfo(ytLink, (err, info) => {
+        if (err) {
+            return res.status(500).send('Error fetching video information');
+        }
+
+        // Get the highest quality audio stream
+        const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
+
+        if (audioFormat) {
+            res.status(200).send(audioFormat.url);
+        } else {
+            res.status(500).send('No suitable audio format found');
+        }
+    });
 });
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: './' });
-})
-
-app.get('/download', (req, res) => {
-    var url = req.query.url;
-    res.header("Content-Disposition", 'attachment; filename="Video.mp4');
-    ytdl(url, {format: 'mp4'}).pipe(res);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
