2cf4bfa6c0f757c329de557be063414b713e34bd
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-18 18:36:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const app = express();
-const port = 3000; // Choose an appropriate port number
+const port = 3000;
 
-app.get('/play', (req, res) => {
-  const youtubeUrl = req.query.url; // Get the YouTube URL from the query parameter
+app.get('/play', async (req, res) => {
+  const youtubeUrl = req.query.url;
+  const timestamp = req.query.t || 0; // Default to 0 if no timestamp is provided
 
   if (!youtubeUrl) {
-    return res.status(400).send('Missing YouTube URL');
+    res.status(400).send('Missing YouTube URL');
+  } else {
+    try {
+      const stream = ytdl(youtubeUrl, { filter: 'audioonly', quality: 'lowestaudio', begin: timestamp });
+      res.set('Content-Type', 'audio/mpeg');
+      stream.pipe(res);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Failed to retrieve audio from YouTube URL');
+    }
   }
-
-  const stream = ytdl(youtubeUrl, { filter: 'audioonly', quality: 'lowestaudio' });
-
-  res.set('Content-Type', 'audio/mpeg');
-  stream.pipe(res);
 });
+
+app.use(express.static('public')); // Serve the HTML file
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
