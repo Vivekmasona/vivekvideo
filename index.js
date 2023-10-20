8610eb92379b5d5ff515c8c796310f1944314793
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-20 19:30:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 const express = require('express');
-const app = express();
 const ytdl = require('ytdl-core');
-const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+const app = express();
 
-app.get('/download', async (req, res) => {
-  const videoUrl = req.query.url; // Get the video URL from the query parameter
-  if (!videoUrl) {
-    res.status(400).send('Please provide a valid video URL');
-    return;
+app.get('/download', (req, res) => {
+  const videoUrl = req.query.url; // The YouTube video URL
+
+  if (!ytdl.validateURL(videoUrl)) {
+    return res.status(400).send('Invalid YouTube URL');
   }
 
-  try {
-    const info = await ytdl.getInfo(videoUrl);
-    const videoStream = ytdl(videoUrl, {
-      quality: 'lowest',
-    });
+  const audio = ytdl(videoUrl, { quality: 'highestaudio' });
 
-    res.header('Content-Disposition', `attachment; filename="${info.videoDetails.title}.mp4"`);
+  res.header('Content-Disposition', 'attachment; filename="audio.mp3"');
+  res.header('Content-Type', 'audio/mpeg');
 
-    // Set the content length header to the size of the video
-    res.header('Content-Length', info.videoDetails.lengthSeconds);
-
-    videoStream.pipe(res);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('An error occurred');
-  }
+  audio.pipe(res);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
+
