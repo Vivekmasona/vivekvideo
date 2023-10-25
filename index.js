31d6c3a760357a40873369fe334b3b512278773e
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-25 20:37:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,31 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
-const fs = require('fs');
 const app = express();
 const port = 3000; // You can change this to the desired port
 
-app.get('/mp4', (req, res) => {
-  // Get the video URL from the query parameter 'url'
+app.get('/mp4', async (req, res) => {
   const videoUrl = req.query.url;
 
-  // Check if the 'url' parameter is provided
   if (!videoUrl) {
     return res.status(400).send('Please provide a valid video URL.');
   }
 
-  // Set the options for downloading audio
-  const options = {
-    quality: 'highestaudio',
-    filter: 'audioonly',
-  };
+  try {
+    // Fetch video details using ytdl-core
+    const info = await ytdl.getInfo(videoUrl);
 
-  // Create a readable stream from the video URL
-  const stream = ytdl(videoUrl, options);
+    // Extract the video title
+    const videoTitle = info.videoDetails.title;
 
-  // Set the filename for the downloaded audio file
-  const filename = 'output.mp3';
-
-  // Create a writable stream to save the audio
-  const output = fs.createWriteStream(filename);
-
-  // Pipe the readable stream to the writable stream
-  stream.pipe(output);
-
-  // Handle the end event when the download is complete
-  output.on('finish', () => {
-    res.download(filename, 'audio.mp3', (err) => {
-      if (err) {
-        console.error('Error:', err);
-        res.status(500).send('An error occurred while downloading the audio.');
-      } else {
-        console.log('Download complete.');
-        // Clean up the temporary file
-        fs.unlinkSync(filename);
-      }
-    });
-  });
-
-  // Handle errors
-  stream.on('error', (err) => {
-    console.error('Error:', err);
-    res.status(500).send('An error occurred while processing the video URL.');
-  });
+    res.send(`Song Title: ${videoTitle}`);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred while fetching video details.');
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
 
