39d9a04486ce88aee424021f4cb62d29d22bc989
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-25 22:06:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,44 @@
+
 const express = require('express');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
 const app = express();
-const port = 3000; // You can change this to the desired port
+const port = 3000;
 
-app.get('/mp4', (req, res) => {
-  // Get the video URL from the query parameter 'url'
+app.get('/mp4', async (req, res) => {
   const videoUrl = req.query.url;
 
-  // Check if the 'url' parameter is provided
   if (!videoUrl) {
     return res.status(400).send('Please provide a valid video URL.');
   }
 
-  // Set the options for downloading audio
+  // Get video info to extract the title
+  const info = await ytdl.getInfo(videoUrl);
+  const videoTitle = info.videoDetails.title;
+
   const options = {
     quality: 'highestaudio',
     filter: 'audioonly',
   };
 
-  // Create a readable stream from the video URL
   const stream = ytdl(videoUrl, options);
-
-  // Set the filename for the downloaded audio file
   const filename = 'output.mp3';
-
-  // Create a writable stream to save the audio
   const output = fs.createWriteStream(filename);
 
-  // Pipe the readable stream to the writable stream
   stream.pipe(output);
 
-  // Handle the end event when the download is complete
   output.on('finish', () => {
-    res.download(filename, 'audio.mp3', (err) => {
+    res.download(filename, `${videoTitle}.mp3`, (err) => {
       if (err) {
         console.error('Error:', err);
         res.status(500).send('An error occurred while downloading the audio.');
       } else {
         console.log('Download complete.');
-        // Clean up the temporary file
         fs.unlinkSync(filename);
       }
     });
   });
 
-  // Handle errors
   stream.on('error', (err) => {
     console.error('Error:', err);
     res.status(500).send('An error occurred while processing the video URL.');
@@ -55,4 +48,3 @@ app.get('/mp4', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
