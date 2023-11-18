89637da9a79b9997328b35c4d0627988b588aa03
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-11-18 20:28:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,20 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
 const ytdl = require("ytdl-core");
-const os = require("os");
-const Utils = require("./src/utils");
-require("dotenv").config();
 
-const app = express();
-const port = process.env.PORT || 4000;
+export default async function handler(req, res) {
+  const { url } = req.query;
 
-const dataPath = path.join(os.tmpdir(), "data");
+  if (!url) return res.status(400).json({ error: "No YouTube link provided" });
 
-Utils.createDir(dataPath);
-
-app.use("/data", express.static(dataPath));
-app.use(express.static(__dirname + "/public"));
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
-
-app.get("/download", async (req, res) => {
-    // Extract the YouTube URL from the 'url' query parameter
-    const youtubeUrl = req.query.url;
-
-    // Check if the 'url' parameter is provided
-    if (!youtubeUrl) {
-        return res.status(400).json({ error: "Missing 'url' parameter" });
-    }
-
-    const sessionID = Date.now(); // Generate a unique session ID
-    const sessionDir = path.join(dataPath, sessionID);
-    const fullSessionDirPath = path.join(dataPath, sessionID);
-
-    Utils.createDir(sessionDir);
-
-    setTimeout(() => {
-        fs.rmSync(sessionDir, { recursive: true });
-    }, 180 * 1000);
-
-    try {
-        const info = await Utils.getInfo(youtubeUrl);
-
-        info.endpointSongPath = path.join(sessionDir, info.filename);
-        info.songPath = path.join(fullSessionDirPath, info.filename);
-        info.fullSessionDirPath = fullSessionDirPath;
-
-        await Utils.downloadSong(info, res);
-    } catch (error) {
-        return res.status(500).json({ error: "Failed to download the audio" });
-    }
-});
-
-// The rest of your existing code for getInfo, clearBucket, bucketItem, getStats, and updateVisitStats endpoints goes here
+  try {
+    ytdl(url, {
+      filter: function (format) {
+        return !format.hasVideo;
+      },
+      quality: "highest",
+    }).pipe(res);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+}
 
