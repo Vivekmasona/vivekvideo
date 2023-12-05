be957ad6071be5fc1773f6c9f4d811030be1961f
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-05 07:55:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-const ytdl = require("ytdl-core");
+const express = require('express');
+const youtubedl = require('youtubedl-core');
+const fs = require('fs');
 
-export default async function handler(req, res) {
-  const { url } = req.query;
+const app = express();
+const port = 3000;
 
-  if (!url) return res.status(400).json({ error: "No YouTube link provided" });
+app.get('/videodl', (req, res) => {
+  const videoUrl = req.query.url;
 
-  try {
-    ytdl(url, {
-      filter: function (format) {
-        return !format.hasVideo;
-      },
-      quality: "highest",
-    }).pipe(res);
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ error: "Something went wrong" });
+  if (!videoUrl) {
+    return res.status(400).send('Video URL is missing.');
   }
-}
+
+  const options = {
+    format: 'best',
+  };
+
+  const video = youtubedl(videoUrl, options);
+
+  res.setHeader('Content-Disposition', 'attachment; filename=output.mp4');
+  video.pipe(res);
+
+  video.on('end', () => {
+    console.log('Download complete!');
+  });
+
+  video.on('error', (error) => {
+    console.error(`Error: ${error.message}`);
+    res.status(500).send('Internal Server Error');
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
